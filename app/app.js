b0e0c5a6a4e7ec7d39e133b2b6a8cd4682e95a7c
CarlosFastLab <fastcaf@hotmail.com>
2023-03-17 15:43:26 -0300

    :sparkles: :lipstick: Small responsive design, adding progress indicator

--- app/app.js
+++ app/app.js
@@ -3,13 +3,69 @@ const businessField = document.getElementById('fbusiness')
 const emailField = document.getElementById('femail')
 const submitBtn = document.getElementById('submit-button')
 const cardCtr = document.getElementById('response-cards')
+const sideBarTriggerBtn = document.getElementById('sidebar-trigger-button')
+const sideBarCloseBtn = document.getElementById('sidebar-close-button')
 
 function isNotImportant (text) {
   return text.includes('NOT IMPORTANT')
 }
 
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "150px";
+  document.getElementById("mySidebar").style.marginLeft = 0;
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
+
+function renderButtonLoader () {
+  console.log('render button loader called')
+  submitBtn.disabled = true
+  submitBtn.classList.add('disabled-button')
+  submitBtn.innerHTML = `
+    <span class="loader"></span>
+  `
+}
+
+function renderButtonContent () {
+  submitBtn.disabled = false
+  submitBtn.classList.remove('disabled-button')
+  submitBtn.innerHTML = `
+    Is it important?
+  `
+}
+
+function showSnackbar() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar")
+
+  // Add the "show" class to DIV
+  x.className = "show"
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000)
+}
+
+sideBarTriggerBtn.addEventListener('click', () => {
+  openNav()
+})
+
+sideBarCloseBtn.addEventListener('click', () => {
+  closeNav()
+})
+
 submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+  e.preventDefault()
+  if (businessField.value === '' || businessField.value === ' ' || emailField.value === '' || emailField.value === ' ') {
+    showSnackbar()
+    return
+  }
+
+  renderButtonLoader()
+
   cardCtr.innerHTML = ''
   console.log('submit button clicked!', businessField.value)
   console.log('Business field value', businessField.value)
@@ -63,6 +119,7 @@ submitBtn.addEventListener('click', (e) => {
           }
         })
       })
+      renderButtonContent()
     })
   } catch (err) {
     console.log(err)
