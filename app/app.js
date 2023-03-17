acf1fea625bbf5c94ca2e60050d09a36383d07a8
CarlosFastLab <fastcaf@hotmail.com>
2023-03-17 17:22:21 -0300

    Getting previous code back, small style adjustment to carsd

--- app/app.js
+++ app/app.js
@@ -3,18 +3,70 @@ const businessField = document.getElementById('fbusiness')
 const emailField = document.getElementById('femail')
 const submitBtn = document.getElementById('submit-button')
 const cardCtr = document.getElementById('response-cards')
+const sideBarTriggerBtn = document.getElementById('sidebar-trigger-button')
+const sideBarCloseBtn = document.getElementById('sidebar-close-button')
 
-function isNotImportant (text) {
+function isNotImportant(text) {
   return text.includes('NOT IMPORTANT')
 }
 
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  cardCtr.innerHTML = ''
-  console.log('submit button clicked!', businessField.value)
-  console.log('Business field value', businessField.value)
-  console.log('Email field value', emailField.value)
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "150px";
+  document.getElementById("mySidebar").style.marginLeft = 0;
+}
 
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
+
+function renderButtonLoader() {
+  console.log('render button loader called')
+  submitBtn.disabled = true
+  submitBtn.classList.add('disabled-button')
+  submitBtn.innerHTML = `
+    <span class="loader"></span>
+  `
+}
+
+function renderButtonContent() {
+  submitBtn.disabled = false
+  submitBtn.classList.remove('disabled-button')
+  submitBtn.innerHTML = `
+    Is it important?
+  `
+}
+
+function showSnackbar() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar")
+
+  // Add the "show" class to DIV
+  x.className = "show"
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000)
+}
+
+sideBarTriggerBtn.addEventListener('click', () => {
+  openNav()
+})
+
+sideBarCloseBtn.addEventListener('click', () => {
+  closeNav()
+})
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (businessField.value === '' || businessField.value === ' ' || emailField.value === '' || emailField.value === ' ') {
+    showSnackbar()
+    return
+  }
+
+  renderButtonLoader()
+
+  cardCtr.innerHTML = ''
   const fetchBody = {
     variables: {
       input: emailField.value,
@@ -22,7 +74,6 @@ submitBtn.addEventListener('click', (e) => {
     },
     user: "carlosaugustofast@gmail.com"
   }
-
   const fetchObject = {
     method: "POST",
     headers: {
@@ -31,40 +82,50 @@ submitBtn.addEventListener('click', (e) => {
     },
     body: JSON.stringify(fetchBody)
   }
-
   try {
     fetch("https://www.everyprompt.com/api/v0/calls/llm-workspace/email-evaluator-KJWqI6", fetchObject)
-    .then((response) => {
-      response.json()
-      .then((responseJson) => {
-        responseJson.completions.forEach((completion) => {
-        if (isNotImportant(completion.text)) {
-          cardCtr.innerHTML += `
-          <div class="card-not-important">
-            <div class="card-header">
-              <h4>✔️ Not Important ✔️</h4>
-            </div>
-            <div class="card-body">
-              <p>${completion.text}</p>
-            </div>
-          </div>
-        `
-        } else {
-          cardCtr.innerHTML += `
-            <div class="card-important">
-              <div class="card-header">
-                <h4>❗ Important ❗</h4>
-              </div>
-              <div class="card-body">
-                <p>${completion.text}</p>
-              </div>
-            </div>
-          `
-          }
-        })
+      .then((response) => {
+        response.json()
+          .then((responseJson) => {
+            responseJson.completions.forEach((completion) => {
+              if (isNotImportant(completion.text)) {
+                cardCtr.innerHTML += `
+                  <div class="card-not-important">
+                    <div class="card-header">
+                    <h4>✔️ Not Important ✔️</h4>
+                    </div>
+                    <div class="card-body">
+                    <p>${completion.text}</p>
+                    </div>
+                  </div>
+              `
+              } else {
+                cardCtr.innerHTML += `
+                  <div class="card-important">
+                    <div class="card-header">
+                      <h4>❗ Important ❗</h4>
+                    </div>
+                    <div class="card-body">
+                      <p>${completion.text}</p>
+                    </div>
+                  </div>
+                `
+              }
+            })
+          })
+        renderButtonContent()
       })
-    })
   } catch (err) {
     console.log(err)
+
+
+
+
+
+
+
+
+
+
   }
 })
